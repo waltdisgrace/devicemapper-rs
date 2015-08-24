548c64c43d581168d5e27adb9c965e8ab76ad667
Andy Grover <agrover@redhat.com>
2015-08-24 15:28:55 -0700

    Update doc.

---
+++ doc/devicemapper/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["dev_majors","Major numbers used by DM."]],"struct":[["DM","Context needed for communicating with devicemapper."],["Device","A struct containing the device's major and minor numbers"]]});
---
+++ doc/implementors/core/str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['devicemapper'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='devicemapper/struct.Device.html' title='devicemapper::Device'>Device</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
