6f01f22bb6f690aea1cd9b2ae0ce70fc4145d977
Andy Grover <agrover@redhat.com>
2015-10-22 14:36:56 -0700

    Update doc.

---
+++ doc/implementors/core/iter/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['devicemapper'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a class='struct' href='devicemapper/struct.DmFlags.html' title='devicemapper::DmFlags'>DmFlags</a>&gt; for <a class='struct' href='devicemapper/struct.DmFlags.html' title='devicemapper::DmFlags'>DmFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
