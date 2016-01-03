3cef71ff49bbc29a6eeb1bec9155cf2e335a345b
Andy Grover <agrover@redhat.com>
2016-01-03 12:04:29 -0800

    Update doc.

--- doc/implementors/core/fmt/trait.Debug.js
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['devicemapper'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='devicemapper/struct.DmFlags.html' title='devicemapper::DmFlags'>DmFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='devicemapper/enum.StatusType.html' title='devicemapper::StatusType'>StatusType</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='devicemapper/struct.Device.html' title='devicemapper::Device'>Device</a>",];
+implementors['devicemapper'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='devicemapper/struct.DmFlags.html' title='devicemapper::DmFlags'>DmFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='devicemapper/struct.Device.html' title='devicemapper::Device'>Device</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ doc/implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['devicemapper'] = ["impl <a class='trait' href='https://serde-rs.github.io/serde/serde/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='devicemapper/struct.Device.html' title='devicemapper::Device'>Device</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
